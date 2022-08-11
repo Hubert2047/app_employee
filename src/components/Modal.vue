a2de8b3ba866e787ef2e04d2f863ec3e2f0c4edc
peter <p9710311@gmail.com>
2022-08-11 17:56:04 +0800

    20220811

---
+++ src/components/Modal.vue
@@ -0,0 +1,32 @@
+<template>
+    <b-button v-b-modal.modal-prevent-closing size="sm" variant="primary">
+        <b-icon icon="plus" aria-hidden="true"></b-icon>
+    </b-button>
+
+    <b-modal
+        header-bg-variant="secondary"
+        header-text-variant="light"
+        id="modal-prevent-closing"
+        ref="modal"
+        hide-header-close="true"
+        title="增作業人員"
+        @show="resetModal"
+        @hidden="resetModal"
+        modal-ok="yes"
+        @ok="handleOk"
+    >
+        <!-- <form ref="form" @submit.stop.prevent="handleSubmit"> -->
+
+        <form ref="form">
+            <b-form-group label="工號" label-for="name-input" invalid-feedback="Name is required">
+                <b-form-input id="name-input" v-model="employee.id" required></b-form-input>
+            </b-form-group>
+            <b-form-group label="姓名" label-for="name-input" invalid-feedback="Name is required">
+                <b-form-input id="name-input" v-model="employee.name" required></b-form-input>
+            </b-form-group>
+        </form>
+    </b-modal>
+</template>
+<script>
+export default {}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -31,15 +31,21 @@
                                 </b-button>
 
                                 <b-modal
+                                    header-bg-variant="secondary"
+                                    header-text-variant="light"
                                     id="modal-prevent-closing"
                                     ref="modal"
+                                    hide-header-close="true"
                                     title="增作業人員"
                                     @show="resetModal"
                                     @hidden="resetModal"
+                                    modal-ok="yes"
                                     @ok="handleOk"
                                 >
-                                    <form ref="form" @submit.stop.prevent="handleSubmit">
-                                        <!-- <b-form-group
+                                    <!-- <form ref="form" @submit.stop.prevent="handleSubmit"> -->
+
+                                    <form ref="form">
+                                        <b-form-group
                                             label="工號"
                                             label-for="name-input"
                                             invalid-feedback="Name is required"
@@ -56,14 +62,14 @@
                                                 v-model="employee.name"
                                                 required
                                             ></b-form-input>
-                                        </b-form-group> -->
+                                        </b-form-group>
                                     </form>
                                 </b-modal>
                             </div>
                             <!-- edit btn -->
-                            <b-button size="sm" class="mb-2 me-2" variant="secondary">
+                            <!-- <b-button size="sm" class="mb-2 me-2" variant="secondary">
                                 <b-icon icon="pencil-square" aria-hidden="true"></b-icon>
-                            </b-button>
+                            </b-button> -->
                             <!-- delete btn -->
                             <b-button @click="deleteEmployee" size="sm" class="mb-2 me-2" variant="danger">
                                 <b-icon icon="trash" aria-hidden="true"></b-icon>
